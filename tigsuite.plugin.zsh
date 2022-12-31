e92a701560838aff03cfafb64477faff706c2321
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2022-12-31 05:11:42 +0059

    Initial commit.

---
+++ tigsuite.plugin.zsh
@@ -0,0 +1,30 @@
+# -*- mode: sh; sh-indentation: 4; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+
+# Copyright (c) 2022 Sebastian Gniazdowski
+
+# According to the Zsh Plugin Standard:
+# https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+
+0=${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}
+0=${${(M)0:#/*}:-$PWD/${0#.(/|)}}
+
+# Then ${0:h} to get plugin's directory
+
+if [[ ${zsh_loaded_plugins[-1]} != */tigsuite && -z ${fpath[(r)${0:h}]} ]] {
+    fpath+=( "${0:h}" )
+}
+
+# (){…} is to use extglob locally
+
+typeset -gA Plugins
+Plugins[TIG_DIR]="$0:h"
+() {
+    emulate -L zsh -o extendedglob
+    # Standard hash for plugins, to not pollute the namespace
+    export TIG_SUITE_DIR=$Plugins[TIG_DIR] \
+        TIG_SUITE_GL=$Plugins[TIG_DIR]/libexec/ti::global.zsh
+    autoload -Uz $TIG_SUITE_DIR/functions/ti(::|)*~*~(:tN.)
+    ti::new-feature 1 q
+ }
+
+# vim:ft=zsh:tw=80:sw=4:sts=4:et:foldmarker=[[[,]]]
