5de6cf5b5a0f9ba44e2405d360db83dee49db3a7
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2023-01-01 06:24:42 +0059

    WIP (zsh problems)

--- libexec/ti::features.zsh
+++ libexec/ti::features.zsh
@@ -1,3 +1,2 @@
-#!/bin/zsh
-0=${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}
-0=${${(M)0##/*}:-$PWD/$0}
+#!/usr/bin/env zsh
+0=${(%):-%x}
--- tigsuite.plugin.zsh
+++ tigsuite.plugin.zsh
@@ -4,9 +4,7 @@
 
 # According to the Zsh Plugin Standard:
 # https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
-
-0=${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}
-0=${${(M)0:#/*}:-$PWD/${0#.(/|)}}
+0=${(%):-%x}
 
 # Then ${0:h} to get plugin's directory
 
